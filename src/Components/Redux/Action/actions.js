1b1bdcbda3b30ed34ca17a859e12980086bd2056
RamKumar93 <115092135+Ramkumar-r7@users.noreply.github.com>
2023-04-26 15:41:37 +0530

    Delete src directory

--- src/Components/Redux/Action/actions.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_USER = "AUTH_USER";
-export const IS_ENROLLED = "IS_ENROLLED";
-export const IS_LEARNING = "IS_LEARNING";
-export const IS_SUBMITTED = "IS_SUBMITTED";
-export const IS_EVALUATED = "IS_EVALUATED";
